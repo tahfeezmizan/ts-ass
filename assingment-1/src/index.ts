f0a731eff9944c94b8d49abe2f66f192ee4edccc
tahfeezmizan <mizanahmed5316@gmail.com>
2025-05-08 10:14:18 +0600

    refactor: update formatString and filterByRating functions with example calls

--- assingment-1/src/index.ts
+++ assingment-1/src/index.ts
@@ -1,23 +1,39 @@
-
-
 function formatString(input: string, toUpper?: boolean): string {
   return toUpper !== false ? input.toUpperCase() : input.toLowerCase();
 }
 
-formatString("Hello");          // Output: "HELLO"
-formatString("Hello", true);   // Output: "HELLO"
-formatString("Hello", false);  // Output: "hello"
+formatString("Hello");
+formatString("Hello", true);
+formatString("Hello", false);
 
+// -------------
 
-
-
-
-type T = {
-  name: string;
-};
-
-function concatenateArrays(...arrays: T[][]): T[] {
-  return arrays.reduce((acc, array) => acc.concat(array), []);
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
 }
 
-console.log(concatenateArrays([{ name: "a" }], [{ name: "b" }]));
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+  { title: "Book D", rating: 4.8 },
+  { title: "Book X", rating: 3.9 },
+];
+
+filterByRating(books);
+
+
+// ---------------------------
+
+type T = string | number;
+
+function concatenateArrays(...arrays: T[][]): T[] {
+  // return arrays.reduce((acc, array) => acc.concat(array), []);
+  // arrays.forEach((array: T[]) => {console.log(array)}
+}
+
+
+concatenateArrays(["a", "b"], ["c"]); 
+concatenateArrays([1, 2], [3, 4], [5]); 
